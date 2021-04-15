99fb65c287551aa1260fde4b981008bfeffddee2
Akshat Agarwal <51703096+akshat-agarwal21@users.noreply.github.com>
2021-04-15 09:39:37 +0530

    Update Products.js

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../App.css'
+import '../../components/Footer.css'
 import productContent from './product-content'
 import {Link} from 'react-router-dom'
 
@@ -7,14 +8,16 @@ import {Link} from 'react-router-dom'
 export default function Products() {
 
     return (
-        <div className='products'>
+        <div className='home'>
            
 
             {productContent.map(product => (
-                <Link to={`/products-detail/${product.type}`}>
-                <h7>{product.type}</h7>
+                <Link className="article-list-item" to={`/products-detail/${product.type}`}>
+                <h7 className='article-list-head'>{product.type}</h7>
+                <p>{product.content[0].substring(0, 150)}...</p>
                 </Link>
             ))}
+            
            
 
 
